06bed5993ae9a010a9ed9a5db018fd364e42441a
helloserve <helloserve@gmail.com>
2015-07-17 21:51:46 +0200

    some documentation updates

--- helloserve.com.Trees.Core/Interfaces/ITree.cs
+++ helloserve.com.Trees.Core/Interfaces/ITree.cs
@@ -6,11 +6,35 @@ using System.Threading.Tasks;
 
 namespace helloserve.com.Trees.Core.Interfaces
 {
+    /// <summary>
+    /// A common interface for a tree implementation.
+    /// </summary>
+    /// <typeparam name="T">The type of the value that will be stored at each node</typeparam>
     public interface ITree<T>
     {
+        /// <summary>
+        /// The root leaf of the tree.
+        /// </summary>
         ILeaf<T> Leaf { get; set; }
+
+        /// <summary>
+        /// Adds an item to the tree.
+        /// </summary>
+        /// <param name="item"></param>
         void Add(T item);
+
+        /// <summary>
+        /// Adds a range of items to the tree.
+        /// </summary>
+        /// <param name="collection"></param>
         void AddRange(IEnumerable<T> collection);
+
+        /// <summary>
+        /// Traverses the tree and returns the values stored at each node in the specified order.
+        /// </summary>
+        /// <param name="mode">The traverse mode to use</param>
+        /// <param name="order">The traverse order to use. Some orders are not applicable to some modes.</param>
+        /// <returns>The list of values stored at the nodes.</returns>
         IList<T> Traverse(TreeTraverseMode mode, TreeTraverseOrder order);
     }
 }
